02c239e4acd011c41f13ed614ac1e71179182799
michalplebanski <32018412+michalplebanski@users.noreply.github.com>
2018-05-01 21:30:38 +0200

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,14 +2,14 @@ import React from 'React';
 import style from './TodoList.css';
 
 const List = props => {
-	const ListElements = props.list.map(element => 
+	const listElements = props.list.map(element => 
 		<li key={element.id} onClick={() => props.delete(element.id)}>
 			{element.text}
 		</li>
 	);
 	return (
 		<ul className={style.TodoList}>
-			{ListElements}
+			{listElements}
 		</ul>
 	);
 }
